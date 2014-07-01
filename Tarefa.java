a3fe402dfe59fc87c20c54028b9986a2e61617b9
jramalho <totoramalho@gmail.com>
2014-06-30 22:52:55 -0300

    Create Tarefa.java

---
+++ Tarefa.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Agenda;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jol
+ */
+public class Tarefa extends Evento {
+
+    public Tarefa(int idEvento, String nomeEvento, String desc, Date dataevent) {
+        super(idEvento, nomeEvento, desc, dataevent);
+    }
+
+}
