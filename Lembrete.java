7d560d436510929d936e6c24d10cbfddd7c4bfd4
jramalho <totoramalho@gmail.com>
2014-06-30 22:52:15 -0300

    Create Lembrete.java

---
+++ Lembrete.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Agenda;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jol
+ */
+public class Lembrete extends Evento {
+
+    public Lembrete(int idEvento, String nomeEvento, String desc, Date data) {
+        super(idEvento, nomeEvento, desc, data);
+    }
+
+
+
+    
+}
