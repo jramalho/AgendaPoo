5364fe47be5953c09ef2ba0e129e25a9dcd5249a
jramalho <totoramalho@gmail.com>
2014-06-30 22:51:57 -0300

    Create Lazer.java

---
+++ Lazer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Agenda;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jol
+ */
+public class Lazer extends Evento {
+
+    public Lazer(int idEvento, String nomeEvento, String desc, Date data) {
+        super(idEvento, nomeEvento, desc, data);
+    }
+
+}
